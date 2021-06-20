c82ce6bc870533b6d76c2443bcb8b1d395126c97
Tatakinov <tatakinov@gmail.com>
2021-06-20 21:51:32 +0900

    codepageを追加しやすいようにした。

--- ghost/master/corelib/conv/windows_wrap.lua
+++ ghost/master/corelib/conv/windows_wrap.lua
@@ -3,7 +3,19 @@ local Conv  = require("conv.windows")
 local M = {}
 
 function M.conv(input, to, from)
-  local output  = Conv.conv(input, to, from)
+  local tbl = {
+    from  = from,
+    to  = to,
+  }
+  for k, v in pairs(tbl) do
+    if v == "Shift_JIS" or v == "cp932" or v == "CP932" then
+      tbl[k]  = 932
+    elseif v == "UTF-8" then
+      tbl[k]  = 65001
+    end
+  end
+
+  local output  = Conv.conv(input, tbl.to, tbl.from)
   if output and #output > 0 then
     return output
   end
