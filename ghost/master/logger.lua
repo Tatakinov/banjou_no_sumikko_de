8df17b09c4a4ebd5820f20e913f31cd3e0acc138
Tatakinov <tatakinov@gmail.com>
2021-08-21 22:27:16 +0900

    起動時のメッセージを変更。複数クライアントのlogを受信できるようにした。

--- ghost/master/logger.lua
+++ ghost/master/logger.lua
@@ -15,23 +15,42 @@ package.cpath = path .. "?." .. ext .. ";" ..
                 path .. "lib/?/init." .. ext
 
 print([[* Kagari/Kotori Logger v1.0.0
-
 ]])
 
 local Socket  = require("socket")
 local server  = assert(Socket.bind("localhost", 49801))
 local ip, port  = server:getsockname()
 
-print("Listen to", ip, port)
+print("* Listen to", ip, port, "\n")
+
+local socs  = {server}
 
 while true do
-  local client  = server:accept()
-  while true do
-    local line, err = client:receive("*l")
-    if err then
-      break
+  local rsocs, wsocs, err = Socket.select(socs, nil, nil)
+  if err then
+    print("An Error Occured")
+    break
+  end
+  for _, v in ipairs(rsocs) do
+    if v == server then
+      local client  = server:accept()
+      table.insert(socs, client)
     else
-      print(client, line)
+      local client  = v
+      local line, err = client:receive("*l")
+      if err then
+        for i, v in ipairs(socs) do
+          if v == client then
+            table.remove(socs, i)
+          end
+        end
+      else
+        print(client, line)
+      end
     end
   end
+end
+
+for _, v in ipairs(socs) do
+  v:close()
 end
